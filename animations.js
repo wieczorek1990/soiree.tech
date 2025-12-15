3046a5558bae058328e8876bac4795cc88379bd3
Łukasz Wieczorek <luke@soiree.tech>
2025-12-15 15:44:50 +0100

    Add typist text thanks.

--- animations.js
+++ animations.js
@@ -6,23 +6,48 @@ const fastDuration = 200;
 const slowDuration = 600;
 const logoDuration = 11 * slowDuration + fastDuration;
 
+$.fn.retype = function (delay) {
+  var el = this,
+    t = el.text(),
+    c = t.split(""),
+    l = c.length,
+    i = 0;
+  delay = delay || 100;
+  el.empty();
+  var interval = setInterval(function () {
+    if (i < l) el.text(el.text() + c[i++]);
+    else clearInterval(interval);
+  }, delay);
+};
+
 function fadeIn(args, callback, duration = "slow") {
   const $element = args[0];
+
   $element.css("visibility", "visible").hide().fadeIn(duration, callback);
 }
 
 function highlight(args, callback) {
   const $highlight = args[0];
+
   $highlight.addClass("highlight");
   setTimeout(callback, slowDuration);
 }
 
 function underline(args, callback) {
   const $mailto = args[0];
+
   $mailto.addClass("underline");
   setTimeout(function () {
     $mailto.removeClass("underline");
+    setTimeout(callback, slowDuration);
   }, slowDuration + fastDuration);
+}
+
+function typistText(args, callback) {
+  const $target = args[0];
+
+  $target.css("visibility", "visible");
+  $target.retype();
 }
 
 $(document).ready(function () {
@@ -38,6 +63,7 @@ $(document).ready(function () {
   const $name = $("#name");
   const $dot = $("#dot");
   const $mailto = $("#mailto");
+  const $thanks = $("#thanks");
 
   fadeIn([$logo], function () {}, logoDuration);
   Sequently.sequence(
@@ -53,6 +79,7 @@ $(document).ready(function () {
       fadeIn,
       fadeIn,
       underline,
+      typistText,
     ],
     [
       [$busy],
@@ -66,6 +93,7 @@ $(document).ready(function () {
       [$name],
       [$dot],
       [$mailto],
+      [$thanks],
     ],
   );
 });
